c9e4f844353a735b791552635c0628c725e0d524
Juan Hualampa <juan.hualampa@7puentes.com>
2026-04-23 17:41:21 -0300

    cors config added

--- src/main/java/ar/edu/unq/backend/config/SecurityConfig.java
+++ src/main/java/ar/edu/unq/backend/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import ar.edu.unq.backend.auth.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,6 +21,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
 
         http
+                .cors(Customizer.withDefaults())
                 .sessionManagement(sm ->
                         sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
