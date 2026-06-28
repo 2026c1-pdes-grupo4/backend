15bd79b6bf3a14932748ce84d5e96757681c5986
Sofia Justiniano <sjustiniano@cdainfo.com>
2026-06-28 18:55:17 -0300

    config sonar cloud + pipelines + observability + readme

--- src/main/java/ar/edu/unq/backend/config/SecurityConfig.java
+++ src/main/java/ar/edu/unq/backend/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
